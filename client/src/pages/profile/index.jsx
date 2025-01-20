e3983cb9c21ce64fb6f75d9330c6c631633e23db
onslaught7 <krutibashmohapatra7@gmail.com>
2025-01-20 16:55:53 +0530

    profile ui done

--- client/src/pages/profile/index.jsx
+++ client/src/pages/profile/index.jsx
@@ -4,9 +4,11 @@ import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { IoArrowBack } from 'react-icons/io5'
 import { Avatar } from '@/components/ui/avatar'
-import { getColor } from '../../lib/utils'
-import { FaTrash } from "react-icons/fa"
-import { FaPlus } from "react-icons/fa"
+import { colors, getColor } from '../../lib/utils'
+import { FaTrash, FaPlus } from "react-icons/fa"
+import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button"
+// import { FaPlus } from "react-icons/fa"
 
 const Profile = () => {
   const navigate = useNavigate();
@@ -38,7 +40,7 @@ const Profile = () => {
                 image ? (
                   <AvatarImage src={image} alt="profile" className='object-cover w-full h-full bg-black'/>
                 ) : ( 
-                  <div className={`upercase h-32 w-32 md:w-48 md:h-48 text-5xl border-[1px] flex items-center justify-center rounded-full ${getColor(selectColor)}`}>
+                  <div className={`uppercase h-32 w-32 md:w-48 md:h-48 text-5xl border-[1px] flex items-center justify-center rounded-full ${getColor(selectColor)}`}>
                     {
                       firstName 
                       ? firstName.split("").shift()
@@ -50,14 +52,73 @@ const Profile = () => {
             </Avatar>
             {
               hovered && (
-                <div className='absolute'>
+                <div className='absolute inset-0 flex items-center justify-center bg-black/50 rounded-full'>
                   {
-                    image ? <FaTrash/> : <FaPlus/>
+                    image ? (
+                      <FaTrash className='text-white text-3xl cursor-pointer'/> 
+                    ) : (
+                      <FaPlus className='text-white text-3xl cursor-pointer'/>
+                    )
                   }
                 </div>
               )
             }
           </div>
+          {/* <input/> */}
+          <div className='flex m-w-32 md:m-w-64 flex-col gap-5 text-white items-center justify-center'>
+            <div className='w-full'>
+              <Input 
+                placeholder="Email"
+                type="email"
+                disabled
+                value={userInfo.email}
+                className="rounded-lg p-6 bg-[#2c2e3b] border-none"
+              />
+            </div>
+            <div className='w-full'>
+              <Input 
+                placeholder="First Name"
+                type="text"
+                onChange={(e) => setFirstName(e.target.value)}
+                value={firstName}
+                className="rounded-lg p-6 bg-[#2c2e3b] border-none"
+              />
+            </div>
+            <div className='w-full'>
+              <Input 
+                placeholder="Last Name"
+                type="text"
+                onChange={(e) => setLastName(e.target.value)}
+                value={lastName}
+                className="rounded-lg p-6 bg-[#2c2e3b] border-none"
+              />
+            </div>
+            <div className='full flex gap-5'>
+              {
+                colors.map((color, index) => 
+                  <div
+                    className={
+                      `${color} h-8 w-8 rounded-full cursor-pointer transition-all duration-300
+                      ${
+                        selectColor === index 
+                        ? "outline outline-white/80 outline-2"
+                        : ""
+                      }`
+                    }
+                    key={ index }
+                    onClick={() => setSelectColor(index)}
+                  ></div>
+                )
+              }
+            </div>
+          </div>
+        </div>
+        <div className='w-full'>
+          <Button className="h-16 w-full bg-purple-700 hover:bg-purple-900 transition-all duration-300"
+            onClick={saveChanges}
+          >
+            save changes
+          </Button>
         </div>
       </div>
     </div>
