7652d6793dc5a4e98c8edae26cca1766a5629a81
Krutibash Mohapatra <krutibashmohapatra7@gmail.com>
2025-02-04 18:36:13 +0530

    chat ui intial steup

--- client/src/pages/chat/index.jsx
+++ client/src/pages/chat/index.jsx
@@ -3,6 +3,9 @@ import { useAppStore } from '@/store'
 import { useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { toast } from 'sonner'
+import ContactsContainer from './components/contacts-container'
+import EmptyChatContainer from './components/empty-chat-container'
+import ChatContainer from './components/chat-container'
 
 const Chat = () => {
   const { userInfo } = useAppStore();
@@ -17,7 +20,11 @@ const Chat = () => {
 
 
   return (
-    <div>Chat</div>
+    <div className='flex h-[100vh] text-white overflow-hidden'>
+      <ContactsContainer/>
+      <EmptyChatContainer/>
+      <ChatContainer/>
+    </div>
   )
 }
 
